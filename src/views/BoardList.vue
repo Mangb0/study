7236587969bb6963aa953364a6951f7330b9f6db
Mangbo <whdudghks45629@gmail.com>
2023-06-19 17:52:31 +0900

    error modalStat = true not working

--- src/views/BoardList.vue
+++ src/views/BoardList.vue
@@ -6,13 +6,22 @@
       <p>{{ board.description }}</p>
     </div>
     <button @click="addBoard">Add Board</button>
+    <button @class="modalStat = true">X</button>
+  </div>
+  <div class="black-bg" v-if="modalStat">
+    <div class="white-bg">
+      <h4>Plese select board Type</h4>
+      <select></select>
+      <button @click="modalStat = false">X</button>
+    </div>
   </div>
 </template>
 
 <script>
-import { reactive } from "vue";
+import { reactive, ref } from "vue";
 export default {
   setup() {
+    const modalStat = ref(true);
     const boards = reactive([
       {
         id: 1,
@@ -40,9 +49,29 @@ export default {
       });
     };
 
-    return { boards, addBoard };
+    return { boards, modalStat, addBoard };
   },
 };
 </script>
 
-<style></style>
+<style>
+body {
+  margin: 0;
+}
+div {
+  box-sizing: border-box;
+}
+.black-bg {
+  width: 100%;
+  height: 100%;
+  background: rgba(0, 0, 0, 0.5);
+  position: fixed;
+  padding: 20px;
+}
+.white-bg {
+  width: 50%;
+  background: white;
+  border-radius: 8px;
+  padding: 20px;
+}
+</style>
