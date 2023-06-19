0c38caca9607c88f02f4843430f5d58f162b26df
Mangbo <whdudghks45629@gmail.com>
2023-06-19 17:49:56 +0900

    add footer

--- src/App.vue
+++ src/App.vue
@@ -5,14 +5,17 @@
   </nav> -->
   <Header />
   <router-view />
+  <Footer />
 </template>
 
 <script>
 import Header from "./components/Header.vue";
+import Footer from "./components/Footer.vue";
 export default {
   name: "App",
   components: {
     Header,
+    Footer,
   },
 };
 </script>
---
+++ src/components/Footer.vue
@@ -0,0 +1,41 @@
+<template>
+  <div class="footer">
+    <h1>Footer</h1>
+    <div>
+      <p>공부시작</p>
+      <button @click="onClickTimer">타이머 시작</button>
+    </div>
+    <h3>{{ study }}</h3>
+    <h3>공부한 시간: {{ studyTime }}초</h3>
+  </div>
+</template>
+
+<script>
+import { ref } from "vue";
+
+export default {
+  setup() {
+    const study = ref(false);
+    let studyStart;
+    let studyEnd;
+    let studyTime = ref(0);
+
+    const onClickTimer = () => {
+      if (study.value) {
+        studyEnd = new Date();
+        console.log(studyEnd);
+        studyTime.value += Math.floor(studyEnd / 10 - studyStart / 10) / 100;
+        alert(Math.floor(studyEnd / 10 - studyStart / 10) / 100 + "초");
+      } else {
+        studyStart = new Date();
+        console.log(studyStart);
+      }
+      study.value = !study.value;
+    };
+    console.log("footer");
+    return { onClickTimer, study, studyTime };
+  },
+};
+</script>
+
+<style></style>
